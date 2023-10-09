d975acf6756f6c00f91bef60bc5d0377d8684a9d
askfiy <c232318108@gmail.com>
2023-10-09 10:06:48 +0800

    chore(null_ls): Optimize variable naming

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ if vim.g.neovide then
     vim.g.neovide_cursor_trail_size = 0.6
 
     vim.g.neovide_cursor_antialiasing = true
-        vim.g.neovide_cursor_animate_in_insert_mode = true
+    vim.g.neovide_cursor_animate_in_insert_mode = true
     vim.g.neovide_cursor_animate_command_line = true
 
     vim.g.neovide_cursor_vfx_mode = "torpedo"
--- lua/core/depends/null_ls/init.lua
+++ lua/core/depends/null_ls/init.lua
@@ -18,29 +18,28 @@ end
 
 function M.load()
     local sources = {}
+
     -- load all sources
     for _, lang_pack in ipairs(api.get_lang().get_lang_pack()) do
         if not vim.tbl_isempty(lang_pack.null_ls) then
-            for source_type, source_package in pairs({
+            for source_type, source_conf in pairs({
                 hover = lang_pack.null_ls.hover,
                 formatting = lang_pack.null_ls.formatting,
                 completion = lang_pack.null_ls.completion,
                 diagnostics = lang_pack.null_ls.diagnostics,
                 code_actions = lang_pack.null_ls.code_actions,
             }) do
-                if source_package and source_package.enable then
-                    local null_ls_builtins_package =
-                        M.null_ls.builtins[source_type][source_package.exe]
-                    if not vim.tbl_isempty(source_package.extra_args) then
-                        null_ls_builtins_package =
-                            M.null_ls.builtins[source_type][source_package.exe].with({
-                                extra_args = source_package.extra_args,
+                if source_conf and source_conf.enable then
+                    local null_ls_pack =
+                        M.null_ls.builtins[source_type][source_conf.exe]
+                    if not vim.tbl_isempty(source_conf.extra_args) then
+                        null_ls_pack =
+                            M.null_ls.builtins[source_type][source_conf.exe].with({
+                                extra_args = source_conf.extra_args,
                             })
                     end
-                    if
-                        not vim.tbl_contains(sources, null_ls_builtins_package)
-                    then
-                        table.insert(sources, null_ls_builtins_package)
+                    if not vim.tbl_contains(sources, null_ls_pack) then
+                        table.insert(sources, null_ls_pack)
                     end
                 end
             end
